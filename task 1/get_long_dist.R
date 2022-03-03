4cd89ef8571ce2086cfec295b3232335456acd3f
Zack Aemmer <zack@Zacks-MacBook-Air.local>
2022-03-03 09:40:16 -0800

    Fix evtrip count

--- task 1/get_long_dist.R
+++ task 1/get_long_dist.R
@@ -34,27 +34,18 @@ rs = dbSendQuery(main_con, insert_query)
 dbClearResult(rs)
 
 # Get only unique OD/DO combos
-all_trips <- DBI::dbGetQuery(main_con,
-                             "SELECT *
-                               FROM (SELECT origin,
-                                            destination,
-                                            SUM(ccounts) AS trip_counts,
-                                            -- Left join leaves zip codes as null if not in wa_bevs
-                                            COALESCE(SUM(evcounts),0) AS bev_counts
-                                    FROM (SELECT
-                                          CASE WHEN origin <= destination THEN origin ELSE destination END AS origin,
-                                          CASE WHEN origin <= destination THEN destination ELSE origin END AS destination,
-                                          ccounts,
-                                          evcounts
-                                          FROM all_trips_count_full
-                                          LEFT JOIN
-                                          (SELECT COUNT(*) AS evcounts,
-                                                  zip_code
-                                          FROM wa_bevs
-                                          WHERE connector_code = 2 OR connector_code = 3
-                                          GROUP BY zip_code) AS wa_bevs
-                                          ON all_trips_count_full.origin = wa_bevs.zip_code) AS ordered
-                                    GROUP BY origin, destination) AS unique_ods;")
+insert_query = paste0(
+  'WITH query1 AS (
+  SELECT origin, destination,
+  SUM(ret) + SUM(dep) AS trips,
+  SUM(oevs)/SUM(ocars) AS o_ratio
+  FROM wa_evtrips
+  GROUP BY origin, destination)
+  SELECT *, o_ratio*trips AS ev_trips
+  FROM query1
+  ORDER BY ev_trips DESC'
+)
+all_trips = dbGetQuery(main_con, insert_query)
 
 cl = makeCluster(10)
 registerDoSNOW(cl)
@@ -78,12 +69,12 @@ progress <- function(n) setTxtProgressBar(pb, n)
 opts <- list(progress = progress)
 
 # Calculate shortest path geometry for each OD and save
-foreach(row=1:nrow(all_trips), .options.snow=opts, .noexport="main_con", .packages=c("DBI","RPostgres")) %dopar% {
+foreach(row=1:100, .options.snow=opts, .noexport="main_con", .packages=c("DBI","RPostgres")) %dopar% {
   insert_query <-
     paste0(
       'INSERT INTO od_sp_geom_wsdot (trip_count, bev_count, od_pairs, geom)
-      (SELECT ',all_trips$trip_counts[row],' AS trip_count,
-              ',all_trips$bev_counts[row],' AS bev_count,
+      (SELECT ',all_trips$trips[row],' AS trip_count,
+              ',all_trips$ev_trips[row],' AS bev_count,
               ',all_trips$origin[row],all_trips$destination[row],' AS od_pairs,
               sq.shortest_path AS geom
       FROM sp_od2(',all_trips$origin[row],', ',all_trips$destination[row],') AS sq)
