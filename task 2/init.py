4912bd180956084e6a37f69bdaa20f1e7d26ec87
Steffen Coenen <steffen.coenen@web.de>
2022-04-04 15:15:29 -0700

    Removed .py files for class structure

--- task 2/init.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def run():
-    import matplotlib
-    
-    matplotlib.pyplot.rcdefaults() #use this to change matplotlib-style-values to default
-    matplotlib.pyplot.style.use(u"../evse.mplstyle")
-    matplotlib.pyplot.close("all")
