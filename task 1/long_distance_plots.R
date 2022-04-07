85a39240a843c8e90513523846b07299bc2a99e8
Zack Aemmer <zae5op@gmail.com>
2022-04-07 10:24:57 -0700

    Plot changes

--- task 1/long_distance_plots.R
+++ task 1/long_distance_plots.R
@@ -5,13 +5,21 @@ library(reshape)
 setwd('/Users/zack/Desktop/stl/wsdot_evse/Task 1')
 ld_data = read.csv('long_distance_counts.csv')
 
+# Remove outliers
 ld_data = ld_data[ld_data$trips<5000,]
-# ld_data = melt(ld_data)
+
+# Separate Upgrade and Candidate sites
+ld_data$station_type = substr(ld_data$pid, 1,1)
+ld_candidate_data = ld_data[ld_data$station_type=='c',]
+ld_upgrade_data = ld_data[ld_data$station_type=='u',]
 
 # Gathered as sum after running shortest path and station analysis, bevs can count towards multiple stations
-ggplot(ld_data, aes(x=trips)) +
+ggplot(ld_candidate_data, aes(x=trips)) +
+  geom_density(color='darkblue', fill='lightblue', bw=300)
+ggplot(ld_upgrade_data, aes(x=trips)) +
   geom_density(color='darkblue', fill='lightblue', bw=300)
 
 # Roughly 5000-10000 in prior analysis with 12% EVs
-pctiles = seq(0,1.0,.25)
-quantile(ld_data$trips, pctiles)
+pctiles = seq(0,1.0,.10)
+quantile(ld_candidate_data$trips, pctiles)
+quantile(ld_upgrade_data$trips, pctiles)
